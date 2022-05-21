ffa00ec2dcb992f4c900d1a5bd113006eec26db7
Rafael Teixeira <rafaeltorress@gmail.com>
2022-05-21 20:35:43 -0300

    Conecting to mongodb with repository and service

---
+++ src/main/java/com/rafael/springmongodb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafael.springmongodb.repository;
+
+import com.rafael.springmongodb.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/rafael/springmongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.rafael.springmongodb.services;
+
+import com.rafael.springmongodb.domain.User;
+import com.rafael.springmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
