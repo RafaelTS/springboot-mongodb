ea9d846790c246df2f6231474ee02538c8ffd126
Rafael Teixeira <rafaeltorress@gmail.com>
2022-06-01 21:19:52 -0300

    Getting a user by id

---
+++ src/main/java/com/rafael/springmongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.rafael.springmongodb.resources.exception;
+
+import com.rafael.springmongodb.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandarError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandarError error = new StandarError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
