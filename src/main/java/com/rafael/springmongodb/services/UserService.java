21a3f4070e787459f41186688d5d1f9107b51256
Rafael Teixeira <rafaeltorress@gmail.com>
2022-06-02 09:47:51 -0300

    implementation of insert method on UserService class

--- src/main/java/com/rafael/springmongodb/services/UserService.java
+++ src/main/java/com/rafael/springmongodb/services/UserService.java
@@ -25,6 +25,10 @@ public class UserService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
