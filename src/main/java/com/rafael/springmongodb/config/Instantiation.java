b8121debdd1e2910b0d51d6e49638293d3c00db5
Rafael Teixeira <rafaeltorress@gmail.com>
2022-06-03 22:23:18 -0300

    Creating AuthorDTO class and projecting data with this class

--- src/main/java/com/rafael/springmongodb/config/Instantiation.java
+++ src/main/java/com/rafael/springmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.rafael.springmongodb.config;
 
 import com.rafael.springmongodb.domain.Post;
 import com.rafael.springmongodb.domain.User;
+import com.rafael.springmongodb.dto.AuthorDTO;
 import com.rafael.springmongodb.repository.PostRepository;
 import com.rafael.springmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para SP, abracos", maria);
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para SP, abracos", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para SP, abracos", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para SP, abracos", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
