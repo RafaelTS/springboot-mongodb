06ab5aa4a5142a006e7d8e0c30fe4f3ab78aa0f9
Rafael Teixeira <rafaeltorress@gmail.com>
2022-06-09 17:49:17 -0300

    querying with multiple criteria

--- src/main/java/com/rafael/springmongodb/resources/util/URL.java
+++ src/main/java/com/rafael/springmongodb/resources/util/URL.java
@@ -1,6 +1,10 @@
 package com.rafael.springmongodb.resources.util;
 
 import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import static java.net.URLDecoder.*;
 
@@ -11,6 +15,16 @@ public class URL {
             return decode(text, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             return "";
+        }
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
         }
     }
 }
